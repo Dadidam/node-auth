933697ce8620ee53cadbfc137aaad364e9a08607
Iliav <iliav@hurdit.com>
2018-08-14 11:22:00 -0400

    Set up node environment

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// Main starting point of the application
+// use `node index.js` command to start node server
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+// Server Setup
+const port = process.env.port || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
